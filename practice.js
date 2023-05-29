0e7206b8642896bff3b227b85df788e64cbc9c95
John Novakowski <jnovakowski797@gmail.com>
2023-05-29 17:49:58 -0400

    third commit for practice

--- practice.js
+++ practice.js
@@ -1,13 +1,29 @@
-console.log('Hello World 2');
-
-console.log('HOORAY');
-
 console.log('Hello World');
 
 
 
 
 
-///////////// Main ///////////
+
+// Presentation on a basic git workflow
+// How to create a branch
+// How to check which branch you're on
+// How to switch between branches
+// How to add remotes
+
+// git branch
+// git checkout
+// git status
+// git commit
+// git log
+// git remote
+// git remote -v
+// git add remote (host name) (host url)
+// git fetch vs pull (you need to fetch and merge)
+
+
+            ///////// Remote version
+            //
+///////////// Main ///////////////////////////
            //
            ///////////"Hello World 2"
